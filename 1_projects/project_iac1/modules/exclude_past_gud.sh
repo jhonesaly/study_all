6039208ecc3a2aaffa610a8a13c9db2d9c3bfda7
Jhones <jhonesaly@gmail.com>
2023-02-11 12:31:57 -0300

    Update exclude_past_gud.sh

--- 1_projects/project_iac1/modules/exclude_past_gud.sh
+++ 1_projects/project_iac1/modules/exclude_past_gud.sh
@@ -1,44 +1,39 @@
 #!/bin/bash
 
-# Excluir diretórios, arquivos, grupos e usuários criados anteriormente
+## Excluir diretórios, arquivos, grupos e usuários criados anteriormente
 
 ## Excluir todos os usuários, exceto o usuário root
 echo "Excluindo usuários..."
+
 for user in $(ls /home); do
   if [ $user != "root" ]; then
-    sudo userdel -r $user
+    sudo userdel -rf $user
   fi
 done
 
 ## Excluir todos os grupos com id superior a 999 e menor que 1100
 echo "Excluindo grupos"
+
 for group in $(cut -d: -f1 /etc/group); do
   id=$(getent group $group | cut -d: -f3)
-  if [ $id -gt 1000 ] && [ $id -lt 1100 ]; then
+  if [ $id -gt 999 ] && [ $id -lt 1100 ]; then
     if [ $group != "sync" ]; then
-      sudo groupdel $group
+      sudo groupdel -f $group
     fi
   fi
 done
 
 ## Excluir todas as pastas dos usuários
 echo "Excluindo pastas"
+
 for user in $(ls /home); do
   sudo rm -rf /home/$user
 done
 
 ## Adicionando novo usuário administrador
 
-read -p "Deseja adicionar um novo usuário administrador? [y/n]" answer
+echo "Adicionando novo usuário administrador"
 
-if [ $answer == "y" ] then;
-  echo "Adicionando novo usuário administrador"
-
-  read -p "Digite o nome do novo usuário administrador: " user_adm
-  adduser $user_adm
-  usermod -aG sudo $user_adm
-
-fi
-
-
-
+read -p "Digite o nome do novo usuário administrador: " user_adm
+adduser $user_adm
+usermod -aG sudo $user_adm
