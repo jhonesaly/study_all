5b215a9c36cd8df2c663e20e52948eeb59d008e0
Jhones <jhonesaly@gmail.com>
2023-02-11 11:11:21 -0300

    Update new_iac.sh

---
+++ 1_projects/project_iac1/.new_iac1b.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# ---
+# Chama o script que exclui antigos diretórios, arquivos, grupos e usuários anteriores
+
+./modules/exclude_past_gud.sh
+
+# ---
+# Chama o script que criar novos diretórios, grupos e usuários
+
+./modules/create_new_gud.sh
+
+# ---
+# Chama o script que altera as permissões das novas pastas
+
+./modules/change_mermissions.sh
