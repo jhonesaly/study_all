167186f5f2e537fae5ffa89708e4eecf81cc358f
Jhones <jhonesaly@gmail.com>
2023-02-11 11:11:40 -0300

    Update new_iac.sh

--- 1_projects/project_iac1/new_iac.sh
+++ 1_projects/project_iac1/new_iac.sh
@@ -4,27 +4,30 @@ echo "Iniciando protocolo new_iac..."
 
 echo "Em caso de dúvida, consulte a documentação disponível em ..."
 
-read -n 1 -p "Deseja que o programa siga estritamente aquilo que foi proposto no projeto? [y/n]" answer_1
+exit = false
 
-if [ "" == ""]; then
-elif [ "" == ""]; then
-else
+while [ "$exit" != true]; do
+
+    read -n 1 -p "Deseja que o programa siga estritamente aquilo que foi proposto no projeto? [y/n]" answer_1
+
+    if [ "$answer_1" == "e"]; then
+        echo "Finalizando..."
+        exit = true
+        break
+
+    elif [ "$answer_1" == "y"]; then
+        echo "Seguindo para protocolo simplificado..."
+        # Caminho do new_iac1a.sh 
+        
+    elif [ "$answer_1" == "n"]; then
+        echo "Seguindo para protocolo avançado..."
+        # Caminho do new_iac1b.sh
+    else
+        echo "Opção inválida. Por favor, escolha 'y' para sim, 'n' para não ou 'e' para sair."
+    fi
+
+done
 
 # ---
-# Chama o script que exclui antigos diretórios, arquivos, grupos e usuários anteriores
 
-./modules/exclude_past_gud.sh
-
-# ---
-# Chama o script que criar novos diretórios, grupos e usuários
-
-./modules/create_new_gud.sh
-
-# ---
-# Chama o script que altera as permissões das novas pastas
-
-./modules/change_mermissions.sh
-
-# ---
-
-echo "End"
+echo "Finalizado"
