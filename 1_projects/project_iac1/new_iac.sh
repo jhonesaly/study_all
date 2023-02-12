41c5f4e3e762311b3a2fa1c1accda6c3984609d3
Jhones <jhonesaly@gmail.com>
2023-02-12 11:11:52 -0300

    Update new_iac.sh

--- 1_projects/project_iac1/new_iac.sh
+++ 1_projects/project_iac1/new_iac.sh
@@ -8,19 +8,21 @@ while true; do
 
     read -n 1 -p "Deseja que o programa siga estritamente aquilo que foi proposto no projeto? [y/n]" answer_1
 
-    if [ "$answer_1" == "e"]; then
+    if [ "$answer_1" == "e" ]; then
         echo "Finalizando..."
         break
 
-    elif [ "$answer_1" == "y"]; then
+    elif [ "$answer_1" == "y" ]; then
         echo "Seguindo para protocolo simplificado..."
         # Caminho do new_iac1a.sh 
         ./.new_iac1a.sh
+        break
         
-    elif [ "$answer_1" == "n"]; then
+    elif [ "$answer_1" == "n" ]; then
         echo "Seguindo para protocolo avançado..."
         # Caminho do new_iac1b.sh
         ./.new_iac1b.sh
+        break
         
     else
         echo "Opção inválida. Por favor, escolha 'y' para sim, 'n' para não ou 'e' para sair."
