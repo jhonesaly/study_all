6a1a64229c8b0b24c375cca49ffcbc1497deab5d
Jhones <jhonesaly@gmail.com>
2023-02-12 11:18:18 -0300

    Update project_iac

--- 1_projects/project_iac1/.new_iac1b.sh
+++ 1_projects/project_iac1/.new_iac1b.sh
@@ -5,9 +5,10 @@
 
 while true; do
     read -n 1 -p "Deseja apagar todos os diretórios, grupos e usuários anteriores? [y/n] " ans_b1
-    if [ $ans_b1 = "y"]; then
+    if [ $ans_b1 = "y" ]; then
         ./modules/exclude_past_gud.sh
-    elif [ $ans_b1 = "n"]; then
+        break
+    elif [ $ans_b1 = "n" ]; then
         break
     else
         echo "Digite um comando válido."
@@ -19,9 +20,10 @@ done
 # Chama o script que criar novos diretórios, grupos e usuários
 while true; do
     read -n 1 -p "Deseja Deseja criar novos diretórios, grupos e usuários anteriores? [y/n] " ans_b2
-    if [ $ans_b2 = "y"]; then
+    if [ $ans_b2 = "y" ]; then
         ./modules/create_new_gud.sh
-    elif [ $ans_b2 = "n"]; then
+        break
+    elif [ $ans_b2 = "n" ]; then
         break
     else
         echo "Digite um comando válido."
