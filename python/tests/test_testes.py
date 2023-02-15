3a54b7132ad92188fd262e216235f5f8d1d40bac
Jhones <jhonesaly@gmail.com>
2023-02-15 20:52:08 -0300

    Update tests with python

--- python/tests/test_testes.py
+++ python/tests/test_testes.py
@@ -1,6 +1,17 @@
 import unittest
+from testes import soma
 
 class TestTestes(unittest.TestCase):
-    pass
+    def test_soma_5_e_5_deve_retornar_10(self):
+        self.assertEqual(soma(5,5), 10)
 
-unittest.main()
+    def test_varias_entradas(self):
+        a_b_saidas = (
+            (10, 10, 20)
+            (10, 10, 11)
+            (5, 5, 10)
+            (1, 1, 3)
+            
+        )
+    
+unittest.main(verbosity=2)
