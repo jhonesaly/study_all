c126ee37404623a043da1d7ebdc4a28c7fadb90d
Jhones <jhonesaly@gmail.com>
2023-03-06 15:04:21 -0300

    Update tests

--- python/algorithm_problems/tests/test_array_three.py
+++ python/algorithm_problems/tests/test_array_three.py
@@ -5,12 +5,15 @@ from array_find_three import *
 
 class TestSearchLinear(unittest.TestCase):
 
-    def test_array_find_three(self):
-        self.assertIn({10,9,8}, test_input)
-        self.assertEqual(array_find_three_func(test_input), {10,9,8})
-        self.assertNotEqual(array_find_three_func(test_input), {1,2,3})
+    def test_array_find_three_equal(self):
+        self.assertEqual(array_find_three_func(test_input), test_answer)
+    
+    def test_array_find_three_notequal(self):
+        self.assertNotEqual(array_find_three_func(test_input), test_not_answer)
 
 if __name__ == '__main__':
-    test_input = [9,8,7,6,5,4,3,2,1,0]
+    test_input = {9,8,7,2,1,0}
+    test_answer = {9,8,7}
+    test_not_answer = {0,1,2}
     unittest.main()
     exit = True
