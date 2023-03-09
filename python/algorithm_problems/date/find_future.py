3e3504ca73860853d06a52dfa7a7eff7a33ed8ed
Jhones <jhonesaly@gmail.com>
2023-03-09 10:11:45 -0300

    feat: Generalize search to a list

--- python/algorithm_problems/date/find_future.py
+++ python/algorithm_problems/date/find_future.py
@@ -6,32 +6,34 @@
 
 import datetime
 
-def find_future(dates_ref_str, search_str):
+def find_future(dates_ref_str, dates_search_str):
     
     dates_ref_list = []
     for date_str in dates_ref_str:
         dates_ref_list.append(datetime.datetime.strptime(date_str, '%d/%m/%Y').date())
     
-    date_search = datetime.datetime.strptime(search_str, '%d/%m/%Y').date()
-    
-    diff_list = []
-    for date_ref in dates_ref_list:
-        diff_date = abs((date_ref - date_search).days)
-        diff_list.append(diff_date)
+    dates_search_list = []
+    for date_str in dates_search_str:
+        dates_search_list.append(datetime.datetime.strptime(date_str, '%d/%m/%Y').date())
 
-    min_diff = min(diff_list)
-    min_index = diff_list.index(min_diff)
-    
-    answer = dates_ref_list[min_index]
+    answer = []
+    for date_search in dates_search_list:
+        diff_list = []
+        for date_ref in dates_ref_list:
+            diff_date = abs((date_ref - date_search).days)
+            diff_list.append(diff_date)
+
+        min_diff = min(diff_list)
+        min_index = diff_list.index(min_diff)
+        answer.append(dates_ref_list[min_index])
     
     return answer
 
 if __name__ == '__main__':
     problem_input_1 = ['22/4/1233', '1/3/0633', '23/5/5665', '4/12/2330'] 
-    problem_input_2 = '23/3/4345'
+    problem_input_2 = ['23/3/4345', '25/5/1244']
 
     problem_output = find_future(problem_input_1, problem_input_2)
     print(problem_output)
-
 
 
