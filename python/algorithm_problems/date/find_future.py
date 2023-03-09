825e5e4b4412a29bece79c567c676d989c1c5664
Jhones <jhonesaly@gmail.com>
2023-03-09 09:51:46 -0300

    test: Add local test

--- python/algorithm_problems/date/find_future.py
+++ python/algorithm_problems/date/find_future.py
@@ -21,29 +21,17 @@ def find_future(dates_ref_str, search_str):
 
     min_diff = min(diff_list)
     min_index = diff_list.index(min_diff)
+    
     answer = dates_ref_list[min_index]
-    print(answer)
-
-    answer = -1
-
+    
     return answer
 
+if __name__ == '__main__':
+    problem_input_1 = ['22/4/1233', '1/3/0633', '23/5/5665', '4/12/2330'] 
+    problem_input_2 = '23/3/4345'
 
-problem_input_1 = ['22/4/1233', '1/3/0633', '23/5/5665', '4/12/2330'] 
-problem_input_2 = '23/3/4345'
+    problem_output = find_future(problem_input_1, problem_input_2)
+    print(problem_output)
 
-find_future(problem_input_1, problem_input_2)
-
-# problem_answer = ['23/5/56645', '4/12/233']
-
-
-
-
-# n = int(input("Enter the number of random dates to generate: "))
-
-# if ...:
-
-# else:
-#     problem_output = -1
 
 
