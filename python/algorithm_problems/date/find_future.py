1a016317d160ea9a77da15303f7a43736aece156
Jhones <jhonesaly@gmail.com>
2023-03-09 11:06:21 -0300

    feature: Add -1 answer in wrong input

--- python/algorithm_problems/date/find_future.py
+++ python/algorithm_problems/date/find_future.py
@@ -14,26 +14,31 @@ def find_future(dates_ref_str, dates_search_str):
     
     dates_search_list = []
     for date_str in dates_search_str:
-        dates_search_list.append(datetime.datetime.strptime(date_str, '%d/%m/%Y').date())
+        try:
+            date = datetime.datetime.strptime(date_str, '%d/%m/%Y').date()
+            dates_search_list.append(date)
+        except ValueError:
+            dates_search_list.append('error')
 
     answer = []
     for date_search in dates_search_list:
-        diff_list = []
-        for date_ref in dates_ref_list:
-            diff_date = abs((date_ref - date_search).days)
-            diff_list.append(diff_date)
+        if date_search == 'error':
+            answer.append(-1)
+        else:
+            diff_list = []
+            for date_ref in dates_ref_list:
+                diff_date = abs((date_ref - date_search).days)
+                diff_list.append(diff_date)
 
-        min_diff = min(diff_list)
-        min_index = diff_list.index(min_diff)
-        answer.append(dates_ref_list[min_index])
+            min_diff = min(diff_list)
+            min_index = diff_list.index(min_diff)
+            answer.append(dates_ref_list[min_index])
     
     return answer
 
 if __name__ == '__main__':
     problem_input_1 = ['22/4/1233', '1/3/0633', '23/5/5665', '4/12/2330'] 
-    problem_input_2 = ['23/3/4345', '25/5/1244']
+    problem_input_2 = ['aba', '25/5/1244']
 
     problem_output = find_future(problem_input_1, problem_input_2)
     print(problem_output)
-
-
