17283e366c341e4599be5743f8ac40b97b04cec4
Jhones <jhonesaly@gmail.com>
2023-03-10 11:01:35 -0300

    feat: basic code example

---
+++ python/type_annotations/type_annotations.py
@@ -0,0 +1,11 @@
+uma_string: str = "um, dois, três"
+um_inteiro: int = 123
+um_float: float = 1.23
+um_boolean: bool = True
+um_set: set = {1, 2, 3}
+uma_lista: list = [1, 2, 3]
+uma_tupla: tuple = (1, 2, 3)
+um_dicionário: dict = {'um': 1, 'dois': 2, 'três': 3}
+
+uma_string = 123
+print(uma_string)
