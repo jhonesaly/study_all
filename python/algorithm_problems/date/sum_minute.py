ae1646843a8f38f3885e0b75171c80a979a7373a
Jhones <jhonesaly@gmail.com>
2023-03-12 10:20:49 -0300

    feat: treat the arguments

--- python/algorithm_problems/date/sum_minute.py
+++ python/algorithm_problems/date/sum_minute.py
@@ -1,6 +1,11 @@
 # You are given a time T in 24-hour format (hh:mm) and a positive integer K, 
 # you have to tell the time after K minutes in 24-hour time.
+import datetime
 
-def sum_minute (T, K):
-    
+def sum_minute (T:str, K:int|str) -> str:
+    date_hour = datetime.strptime(T, "%H:%M")
+    if isinstance(K, str):
+        K = int(K)
+            
+
     return new_T
