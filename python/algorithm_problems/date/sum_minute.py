ea4a7855b7fa5d2708320ce36d4d4bd695c2a7e1
Jhones <jhonesaly@gmail.com>
2023-03-12 11:30:29 -0300

    refactor: improves variable quality and structure

--- python/algorithm_problems/date/sum_minute.py
+++ python/algorithm_problems/date/sum_minute.py
@@ -2,18 +2,20 @@
 # you have to tell the time after K minutes in 24-hour time.
 import datetime
 
-def sum_minute (T:str, K:int|str) -> str:
-    date_hour = datetime.datetime.strptime(T, "%H:%M")
-    if isinstance(K, str):
+def sum_minute (init_hour_str:str, add_minutes:int|str) -> str:
+    
+    init_date_hour = datetime.datetime.strptime(init_hour_str, "%H:%M")
+    
+    if isinstance(add_minutes, str):
         try:
-            K = int(K)
+            add_minutes = int(add_minutes)
         except ValueError:
             raise ValueError("Invalid value for second argument")
 
-    new_T = date_hour + datetime.timedelta(minutes=K)
-    new_T_str = new_T.strftime("%H:%M")
+    new_date_hour = init_date_hour + datetime.timedelta(minutes=add_minutes)
+    new_date_hour_str = new_date_hour.strftime("%H:%M")
 
-    return new_T_str
+    return new_date_hour_str
 
 if __name__ == "__main__":
     test = sum_minute('20:39', 534)
