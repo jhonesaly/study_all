b483647a4ff5178ce51b44de1fe16f8a94c1ab24
Jhones <jhonesaly@gmail.com>
2023-03-12 10:11:36 -0300

    feat: add format of function

---
+++ python/algorithm_problems/date/sum_minute.py
@@ -0,0 +1,6 @@
+# You are given a time T in 24-hour format (hh:mm) and a positive integer K, 
+# you have to tell the time after K minutes in 24-hour time.
+
+def sum_minute (T, K):
+    
+    return new_T
