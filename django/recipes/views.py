48dcc4ae8989cff0ac8bffeae62c4eedf87340b0
Jhones <jhonesaly@gmail.com>
2023-03-13 17:12:14 -0300

    feat: Create template and namespace

--- django/recipes/views.py
+++ django/recipes/views.py
@@ -4,7 +4,7 @@ from django.http import HttpResponse
 # Create your views here.
 
 def home(request):
-    return HttpResponse('Home')
+    return render(request, 'recipes/home.html')
 
 def sobre(request):
     return HttpResponse('Sobre')
