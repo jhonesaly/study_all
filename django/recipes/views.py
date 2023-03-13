b3f55f2c7c4396e580c071ee34639b7f670fcc86
Jhones <jhonesaly@gmail.com>
2023-03-13 16:43:48 -0300

    feat: Add views of app

--- django/recipes/views.py
+++ django/recipes/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
 # Create your views here.
+
+def home(request):
+    return HttpResponse('Home')
+
+def sobre(request):
+    return HttpResponse('Sobre')
+
+def contato(request):
+    return HttpResponse('Contato')
