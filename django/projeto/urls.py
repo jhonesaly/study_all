fee8aed137da98d3ec22e943e7e1dcc8b5a48bc8
Jhones <jhonesaly@gmail.com>
2023-03-13 16:11:54 -0300

    feat: Add urls

--- django/projeto/urls.py
+++ django/projeto/urls.py
@@ -15,7 +15,23 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from django.http import HttpResponse
+
+# HTTP REQUEST
+def home(request):
+    return HttpResponse('Home')
+
+def sobre(request):
+    return HttpResponse('Sobre')
+
+def contato(request):
+    return HttpResponse('Contato')
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', home), # Home
+    path('sobre/', sobre), # Sobre
+    path('contato/', contato), # Contato
+
 ]
+
