0918c2828be365352cdae9fb7b9d673c8395f6a1
Jhones <jhonesaly@gmail.com>
2023-03-14 14:38:05 -0300

    feat: create header and partials

--- django/recipes/views.py
+++ django/recipes/views.py
@@ -4,6 +4,6 @@ from django.http import HttpResponse
 # Create your views here.
 
 def home(request):
-    return render(request, 'recipes/home.html', context={
+    return render(request, 'recipes/pages/home.html', context={
         'name': 'Jota',
     })
