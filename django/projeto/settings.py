f3f32734199ff3ec21ac8db1fa4035571e59b934
Jhones <jhonesaly@gmail.com>
2023-03-16 15:41:11 -0300

    feat: Add STATIC_ROOT and create global static dir

--- django/projeto/settings.py
+++ django/projeto/settings.py
@@ -105,9 +105,6 @@ AUTH_PASSWORD_VALIDATORS = [
 # https://docs.djangoproject.com/en/4.1/topics/i18n/
 
 LANGUAGE_CODE = 'en-us'
-STATICFILES_DIRS = [
-    BASE_DIR / 'base_static',
-]
 
 
 TIME_ZONE = 'UTC'
@@ -121,6 +118,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_URL = 'static/'
+STATICFILES_DIRS = [
+    BASE_DIR / 'base_static',
+]
+STATIC_ROOT = BASE_DIR / 'static'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
