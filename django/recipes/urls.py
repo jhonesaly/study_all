0d38594ea056707902865ec70f5187117967a8ac
Jhones <jhonesaly@gmail.com>
2023-03-17 15:53:10 -0300

    feat: give unique name for urls.

--- django/recipes/urls.py
+++ django/recipes/urls.py
@@ -3,6 +3,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.home),
-    path('recipes/<int:id>/', views.recipes),
+    path('', views.home, name="recipes-home"),
+    path('recipes/<int:id>/', views.recipes, name="recipes-recipe"),
 ]
