09f7e22c5929894090bca9b85ed5b15b5c7930f6
Jhones <jhonesaly@gmail.com>
2023-03-18 17:05:51 -0300

    feat: change day of reference

--- python/algorithm_problems/date/day_week.py
+++ python/algorithm_problems/date/day_week.py
@@ -3,15 +3,15 @@ import datetime
 def day_week(day_input_str):
     day_input_date = datetime.datetime.strptime(day_input_str, '%d/%m/%Y').date()
     
-    day_date_ref = datetime.date(2023,3,18)
-    day_week_int_ref = 7
+    day_date_ref = datetime.date(1,1,1)
+    day_week_int_ref = 2
 
     diff_days = (day_input_date - day_date_ref).days
     diff_day_week = diff_days % 7
-    day_of_week = diff_day_week
+    day_of_week = diff_day_week + day_week_int_ref
     return day_of_week
 
 if __name__ == '__main__':
-    problem_input_1 = '25/3/2023'
+    problem_input_1 = '18/3/2023'
     problem_output = day_week(problem_input_1)
     print(problem_output)
