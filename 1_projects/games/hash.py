16cc2421777bdf9e64eb9127c9fce639fee35d4d
Jhones <jhonesaly@gmail.com>
2023-03-18 18:50:27 -0300

    feat: create second move of game

--- 1_projects/games/hash.py
+++ 1_projects/games/hash.py
@@ -13,8 +13,10 @@ while end_game == False:
     move1 = move1.split(',')
     move1 = [int(x)-1 for x in move1]
     hash[move1[0]][move1[1]] = 'X'
-        
-    end_game = True
+
+    move2 = move2.split(',')
+    move2 = [int(x)-1 for x in move2]
+    hash[move2[0]][move2[1]] = 'O'
 
     print(hash[0][0])
     print(move1[0])
