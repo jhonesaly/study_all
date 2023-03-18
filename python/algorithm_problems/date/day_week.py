9e35a9e3fb2ace59f54742a90b2b2605883da808
Jhones <jhonesaly@gmail.com>
2023-03-18 16:48:19 -0300

    feat: calculate difference between days

--- python/algorithm_problems/date/day_week.py
+++ python/algorithm_problems/date/day_week.py
@@ -6,10 +6,12 @@ def day_week(day_input_str):
     day_date_ref = datetime.date(2023,3,18)
     day_week_int_ref = 7
 
-    day_of_week = day_date_ref
+    diff_days = (day_input_date - day_date_ref).days
+
+    day_of_week = diff_days
     return day_of_week
 
 if __name__ == '__main__':
-    problem_input_1 = '18/3/2023'
+    problem_input_1 = '20/4/2023'
     problem_output = day_week(problem_input_1)
     print(problem_output)
