e703f83aa8de124e60d4af894008a46c816d9552
Jhones <jhonesaly@gmail.com>
2023-03-18 16:23:54 -0300

    feat: convert date str into pure date

--- python/algorithm_problems/date/day_week.py
+++ python/algorithm_problems/date/day_week.py
@@ -1,8 +1,12 @@
-def day_week(day_str):
-    
-    return day_week
+import datetime
+
+def day_week(input_date_str):
+    mid = datetime.datetime.strptime(input_date_str, '%d/%m/%Y').date()
+    print(mid)
+    day_of_week = []
+    return day_of_week
 
 if __name__ == '__main__':
-    problem_input_1 = ['18/3/2023']
+    problem_input_1 = '18/3/2023'
     problem_output = day_week(problem_input_1)
     print(problem_output)
