c5abc7132a789ba7999a5db67621a36d72bd0299
Jhones <jhonesaly@gmail.com>
2023-03-18 18:23:39 -0300

    feat: add players

--- 1_projects/games/hash.py
+++ 1_projects/games/hash.py
@@ -6,6 +6,10 @@ while end_game == False:
 
     for line in hash:
         print(line)
+    
+    player1 = input("Digite onde deja colocar o X (ex: 1,1): ")
+    player2 = input("Digite onde deja colocar o O (ex: 1,1): ")
+
 
     end_game = True
 
