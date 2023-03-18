99b4497ea49c40bc2dfd206d11b99f50c8c4b4ec
Jhones <jhonesaly@gmail.com>
2023-03-18 18:49:21 -0300

    feat: create move of game

--- 1_projects/games/hash.py
+++ 1_projects/games/hash.py
@@ -1,4 +1,4 @@
-hash = ('','',''),('','',''),('','','')
+hash = ['','',''],['','',''],['','','']
 
 end_game = False
 
@@ -7,9 +7,14 @@ while end_game == False:
     for line in hash:
         print(line)
     
-    player1 = input("Digite onde deja colocar o X (ex: 1,1): ")
-    player2 = input("Digite onde deja colocar o O (ex: 1,1): ")
+    move1 = input("Digite onde deja colocar o X (ex: 1,1): ")
+    move2 = input("Digite onde deja colocar o O (ex: 1,1): ")
 
-
+    move1 = move1.split(',')
+    move1 = [int(x)-1 for x in move1]
+    hash[move1[0]][move1[1]] = 'X'
+        
     end_game = True
 
+    print(hash[0][0])
+    print(move1[0])
