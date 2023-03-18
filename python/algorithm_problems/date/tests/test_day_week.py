2e442c87a75f69e8a61e4f5be42cdedd2b7302fb
Jhones <jhonesaly@gmail.com>
2023-03-18 17:27:56 -0300

    test: create basic unit test

--- python/algorithm_problems/date/tests/test_day_week.py
+++ python/algorithm_problems/date/tests/test_day_week.py
@@ -8,7 +8,24 @@ from day_week import *
 
 class TestDateDayWeek(unittest.TestCase):
 
+    def test_day_week_found(self):
+        input_1 = '18/3/2023'
+        output_actual = day_week(input_1)    
+        output_expected = 7
+        
+        self.assertEqual(output_actual,output_expected)
 
+        input_1 = '19/3/2023'
+        output_actual = day_week(input_1)    
+        output_expected = 1
+        
+        self.assertEqual(output_actual,output_expected)
+
+        input_1 = '17/3/2023'
+        output_actual = day_week(input_1)    
+        output_expected = 6
+        
+        self.assertEqual(output_actual,output_expected)
 
 if __name__ == '__main__':
     unittest.main()
