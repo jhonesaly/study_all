fc9416b6a1e872d8383ebad4e51ac156e0ef2d2e
Jhones <jhonesaly@gmail.com>
2023-03-18 18:20:10 -0300

    feat: add loop of game

--- 1_projects/games/hash.py
+++ 1_projects/games/hash.py
@@ -1,4 +1,11 @@
 hash = ('','',''),('','',''),('','','')
 
-for line in hash:
-    print(line)
+end_game = False
+
+while end_game == False:
+
+    for line in hash:
+        print(line)
+
+    end_game = True
+
