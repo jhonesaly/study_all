bf8349535ec69c626f9bfeec5fa8eeac84caf676
Jhones <jhonesaly@gmail.com>
2023-03-18 17:21:26 -0300

    test: create structure of unit test

---
+++ python/algorithm_problems/date/tests/test_day_week.py
@@ -0,0 +1,14 @@
+import unittest
+import os
+import sys
+
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+
+from day_week import *
+
+class TestDateDayWeek(unittest.TestCase):
+
+
+
+if __name__ == '__main__':
+    unittest.main()
