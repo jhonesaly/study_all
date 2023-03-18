15f7fb9167cfcfa88f1f0a8bb9bac01b9512ce0a
Jhones <jhonesaly@gmail.com>
2023-03-18 16:47:52 -0300

    refactor: better var names

--- python/algorithm_problems/date/day_week.py
+++ python/algorithm_problems/date/day_week.py
@@ -1,12 +1,12 @@
 import datetime
 
-def day_week(day_str):
-    day_date = datetime.datetime.strptime(day_str, '%d/%m/%Y').date()
+def day_week(day_input_str):
+    day_input_date = datetime.datetime.strptime(day_input_str, '%d/%m/%Y').date()
     
-    day_ref = datetime.date(2023,3,18)
-    day_week_ref = 7
+    day_date_ref = datetime.date(2023,3,18)
+    day_week_int_ref = 7
 
-    day_of_week = day_ref
+    day_of_week = day_date_ref
     return day_of_week
 
 if __name__ == '__main__':
