c9865b3978412115059f792851790fba5ecd4ef7
Jhones <jhonesaly@gmail.com>
2023-03-18 16:15:38 -0300

    test: add local test

--- python/algorithm_problems/date/day_week.py
+++ python/algorithm_problems/date/day_week.py
@@ -1,3 +1,8 @@
 def day_week(day_str):
     
     return day_week
+
+if __name__ == '__main__':
+    problem_input_1 = ['18/3/2023']
+    problem_output = day_week(problem_input_1)
+    print(problem_output)
