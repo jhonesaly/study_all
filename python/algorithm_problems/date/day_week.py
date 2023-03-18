0f5506087049c3e3af3da51aa3e13d70e23a27c1
Jhones <jhonesaly@gmail.com>
2023-03-18 16:52:22 -0300

    feat: calculate rest of division

--- python/algorithm_problems/date/day_week.py
+++ python/algorithm_problems/date/day_week.py
@@ -7,11 +7,11 @@ def day_week(day_input_str):
     day_week_int_ref = 7
 
     diff_days = (day_input_date - day_date_ref).days
-
-    day_of_week = diff_days
+    diff_day_week = diff_days % 7
+    day_of_week = diff_day_week
     return day_of_week
 
 if __name__ == '__main__':
-    problem_input_1 = '20/4/2023'
+    problem_input_1 = '25/3/2023'
     problem_output = day_week(problem_input_1)
     print(problem_output)
