fd36a1c2b99f1aa96bc6694becfd99b776434681
Jhones <jhonesaly@gmail.com>
2023-03-18 19:03:02 -0300

    feat: create loop for ok move

--- 1_projects/games/hash.py
+++ 1_projects/games/hash.py
@@ -7,21 +7,29 @@ while end_game == False:
     for line in hash:
         print(line)
     
-    move1 = input("Digite onde deja colocar o X (ex: 1,1): ")
-    move2 = input("Digite onde deja colocar o O (ex: 1,1): ")
+    move1_test = 'not_ok'
 
-    move1 = move1.split(',')
-    move1 = [int(x)-1 for x in move1]
-    if hash[move1[0]][move1[1]] != '':
-        print('Espaço já marcado. Escolha outro!')
-    else:
-        hash[move1[0]][move1[1]] = 'X'
+    while move1_test != 'ok':
+        move1 = input("Digite onde deja colocar o X (ex: 1,1): ")
+        move1 = move1.split(',')
+        move1 = [int(x)-1 for x in move1]
+        if hash[move1[0]][move1[1]] != '':
+            print('Espaço já marcado. Escolha outro!')
+        else:
+            hash[move1[0]][move1[1]] = 'X'
+            move1_test = 'ok'
 
-    move2 = move2.split(',')
-    move2 = [int(x)-1 for x in move2]
-    if hash[move2[0]][move2[1]] != '':
-        print('Espaço já marcado. Escolha outro!')
-    else:
-        hash[move2[0]][move2[1]] = 'O'
+    move2_test = 'not_ok'
+
+    while move2_test != 'ok':
+        move2 = input("Digite onde deja colocar o O (ex: 1,1): ")
+        move2 = move2.split(',')
+        move2 = [int(x)-1 for x in move2]
+        if hash[move2[0]][move2[1]] != '':
+            print('Espaço já marcado. Escolha outro!')
+        else:
+            hash[move2[0]][move2[1]] = 'O'
+            move2_test = 'ok'
+
 
     
