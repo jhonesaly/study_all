4857a9fed998b6c087b23ef4fb74da7ff4e36497
Jhones <jhonesaly@gmail.com>
2023-03-19 21:42:25 -0300

    refactor: better var names and cut some junk

--- python/algorithm_problems/array/find_three.py
+++ python/algorithm_problems/array/find_three.py
@@ -1,44 +1,26 @@
 import os
-from search import *
 
 os.system('cls')
 
 # Criando o input: 
 
-def array_find_three_func(problem_input):
+def find_three(list):
 
-    problem_output = [0,0,0]
+    max_three = [0,0,0]
 
-    for i in problem_input:
-        minimal = min(problem_output)
-        idx = problem_output.index(minimal)
+    for i in list:
+        minimal = min(max_three)
+        idx = max_three.index(minimal)
         if i > minimal:
-            problem_output[idx] = i
+            max_three[idx] = i
 
-    problem_output = set(problem_output)
+    max_three = set(max_three)
 
-    return problem_output
+    return max_three
 
 if __name__ == '__main__':
 
-    problem_input = gen_rand_list(5, 99)
+    problem_input = [3, 31, 63, 74, 15]
+    problem_output = find_three(problem_input)
 
-    print(f'\nentradas: {problem_input}\n')
-
-    problem_output = array_find_three_func(problem_input)
-
-    print(f'\nsaída: {problem_output}\n')
-
-    sorted_list = sorted(problem_input, reverse=True)
-    answer = set(sorted_list[:3])
-
-    print(f'\nresposta: {answer}\n')
-
-    # Teste
-
-    test1 = False
-
-    if answer == problem_output:
-        test1 = True
-
-    print(f'\nTeste 1: {test1}\n')
+    print(problem_output)
