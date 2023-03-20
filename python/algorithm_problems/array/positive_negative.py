39e9cebbf0f2570f40bb11de171edf52ddf7159d
Jhones <jhonesaly@gmail.com>
2023-03-19 21:09:01 -0300

    feat:create structure of function

---
+++ python/algorithm_problems/array/positive_negative.py
@@ -0,0 +1,7 @@
+# Given two integer arrays of same size, “arr[]” and “index[]”, 
+# reorder elements in “arr[]” according to given index array. 
+# It is not allowed to given array arr’s length.
+
+def positive_negative(list_val, list_index):
+    
+    return list_val_ordered, list_index_ordered
