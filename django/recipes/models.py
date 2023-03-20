e9f67b197514e5d14d3b5cc0941233141d06bc4b
Jhones <jhonesaly@gmail.com>
2023-03-20 19:59:24 -0300

    feat: create class author

--- django/recipes/models.py
+++ django/recipes/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 class Category(models.Model):
     name = models.CharField(max_length=65)
@@ -18,3 +19,4 @@ class Recipe(models.Model):
     is_published = models.BooleanField(default=False)
     cover = models.ImageField(upload_to='recipes/covers/%Y/%m/%d/')
     category = models.ForeignKey(Category, on_delete=models.SET_NULL, null=True)
+    author = models.ForeignKey(User, on_delete=models.SET_NULL, null=True)
