bd9ccdac14450c7ececa64c55dc443538ecd5b9a
Jhones <jhonesaly@gmail.com>
2023-03-19 21:47:28 -0300

    test: create structure of test

---
+++ python/algorithm_problems/array/tests/test_positive_negative.py
@@ -0,0 +1,13 @@
+import unittest
+import os
+import sys
+
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+
+from positive_negative import *
+
+if __name__ == '__main__':
+    #test_input = 
+    #test_answer = 
+    #test_not_answer = 
+    unittest.main()
