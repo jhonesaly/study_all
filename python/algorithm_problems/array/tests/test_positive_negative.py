e49a13622416df60d97aaa11c8954682aaae88f7
Jhones <jhonesaly@gmail.com>
2023-03-20 17:19:50 -0300

    test: create structure of unit test

---
+++ python/algorithm_problems/array/tests/test_positive_negative.py
@@ -0,0 +1,14 @@
+import unittest
+import os
+import sys
+
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+
+from positive_negative import *
+
+class TestPositiveNegative(unittest.TestCase):
+    def test_positive_negative_equal(self):
+    
+
+if __name__ == '__main__':
+    unittest.main()
