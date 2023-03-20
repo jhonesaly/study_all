a4d19bc3d77e1c920c3bab5ada6aad41f90a3546
Jhones <jhonesaly@gmail.com>
2023-03-19 22:01:16 -0300

    test: create first kind of test

--- python/algorithm_problems/array/tests/test_positive_negative.py
+++ python/algorithm_problems/array/tests/test_positive_negative.py
@@ -6,8 +6,19 @@ sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 
 from positive_negative import *
 
+class TestPositiveNegative(unittest.TestCase):
+    def test_positive_negative_equal(self):
+        result_1, result_2 = positive_negative(test_input_1, test_input_2)
+        self.assertEqual(result_1, test_answer_1)
+        self.assertEqual(result_2, test_answer_2)
+    
+
 if __name__ == '__main__':
-    #test_input = 
-    #test_answer = 
+    test_input_1 = [10, 11, 12]
+    test_input_2 = [1, 0, 2]
+
+    test_answer_1 = [11, 10, 12]
+    test_answer_2 = [0, 1, 2]
+
     #test_not_answer = 
     unittest.main()
