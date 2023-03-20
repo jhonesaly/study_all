ef57622e67aa1abfe1b8fe41621c55593146655b
Jhones <jhonesaly@gmail.com>
2023-03-19 21:02:14 -0300

    refactor: rename vars for more comprehension

--- python/algorithm_problems/array/reversal.py
+++ python/algorithm_problems/array/reversal.py
@@ -1,35 +1,23 @@
 import os
-from search import *
 
 os.system('cls')
 
 ###
 def reversal(list, search):
-    problem_output = [0 for i in range(search)]
+    list_reversal = [0 for i in range(search)]
 
     for i in range(search):
-        aux_1 = problem_input1.pop(-1)
+        aux_1 = list.pop(-1)
         aux_2 = search - i - 1
-        problem_output[aux_2] = aux_1
+        list_reversal[aux_2] = aux_1
 
-    problem_output.extend(problem_input1)
-    print(f'\nsaída: {problem_output}\n')
-    return problem_output
+    list_reversal.extend(list)
+
+    return list_reversal
 
 ###
 if __name__ == '__main__':
-    problem_input1 = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
-    problem_input2 = 5
-
-
-# answer = [8, 9, 10, 1, 2, 3, 4, 5, 6, 7]
-# print(f'\nresposta: {answer}\n')
-
-# Teste
-
-# test1 = False
-
-# if answer == problem_output:
-#     test1 = True
-
-# print(f'\nTeste 1: {test1}\n')
+    list = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+    problem_input2 = 2
+    problem_output = reversal(list, problem_input2)
+    print(f'\nsaída: {problem_output}\n')
