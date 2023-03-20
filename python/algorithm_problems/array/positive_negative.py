99b52936f487128ff706f22e0c753acda008cfc8
Jhones <jhonesaly@gmail.com>
2023-03-19 21:12:07 -0300

    test: add local test

--- python/algorithm_problems/array/positive_negative.py
+++ python/algorithm_problems/array/positive_negative.py
@@ -5,3 +5,11 @@
 def positive_negative(list_val, list_index):
     
     return list_val_ordered, list_index_ordered
+
+if __name__ == '__main__':
+    input_1 = [10, 11, 12]
+    input_2 = [1, 0, 2]
+
+    output_1, output_2 = positive_negative(input_1, input_2)
+    print(output_1)
+    print(output_2)
