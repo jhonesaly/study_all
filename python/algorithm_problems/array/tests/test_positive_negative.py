336d1392de42d7d76770042b3f636d726043d6c9
Jhones <jhonesaly@gmail.com>
2023-03-20 17:25:21 -0300

    test: create basic test

--- python/algorithm_problems/array/tests/test_positive_negative.py
+++ python/algorithm_problems/array/tests/test_positive_negative.py
@@ -8,7 +8,12 @@ from positive_negative import *
 
 class TestPositiveNegative(unittest.TestCase):
     def test_positive_negative_equal(self):
-    
+        result_1 = positive_negative(test_input_1)
+        self.assertEqual(result_1, test_answer_1)
 
 if __name__ == '__main__':
+    
+    test_input_1 = [-1, 3, -5, 6, 3, 6, -7, -4, -9, 10]
+    test_answer_1 = [3, -1, 6, -5, 3, -7, 6, -4, 10, -9]
+
     unittest.main()
