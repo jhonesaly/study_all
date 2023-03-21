b06da58cc398ca04bde0cdc8ecc4a14cd789d971
Jhones <jhonesaly@gmail.com>
2023-03-21 10:14:34 -0300

    feat: configure django to serve images

--- django/projeto/urls.py
+++ django/projeto/urls.py
@@ -15,7 +15,8 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
-
+from django.conf.urls.static import static
+from django.conf import settings
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -23,3 +24,4 @@ urlpatterns = [
     # path('urls/', include('recipes.urls')), # cominio.com/urls
 ]
 
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
