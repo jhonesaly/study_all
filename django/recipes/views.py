5e2afebce27ad4b749a14c21b78cf103d4b720ef
Jhones <jhonesaly@gmail.com>
2023-03-21 16:55:38 -0300

    fix: solve problem of none author and category

--- django/recipes/views.py
+++ django/recipes/views.py
@@ -1,11 +1,14 @@
+from django.http import Http404
 from django.shortcuts import render
 from utils.recipes.factory import make_recipe
 
-from .models import Recipe
+from recipes.models import Recipe
 
 
 def home(request):
-    recipes = Recipe.objects.all().order_by('-id')
+    recipes = Recipe.objects.filter(
+        is_published=True
+    ).order_by('-id')
     return render(request, 'recipes/pages/home.html', context={
         'recipes': recipes,
     })
@@ -13,10 +16,16 @@ def home(request):
 
 def category(request, category_id):
     recipes = Recipe.objects.filter(
-        category__id=category_id
+        category__id=category_id,
+        is_published=True,
     ).order_by('-id')
-    return render(request, 'recipes/pages/home.html', context={
+
+    if not recipes:
+        raise Http404('Not found 🥲')
+
+    return render(request, 'recipes/pages/category.html', context={
         'recipes': recipes,
+        'title': f'{recipes.first().category.name} - Category | '
     })
 
 
