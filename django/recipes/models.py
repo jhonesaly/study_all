fbe8b48051080342925689b378ce2f5169b66870
Jhones <jhonesaly@gmail.com>
2023-03-21 09:36:37 -0300

    feat: show name of category in CRUD

--- django/recipes/models.py
+++ django/recipes/models.py
@@ -4,6 +4,9 @@ from django.contrib.auth.models import User
 class Category(models.Model):
     name = models.CharField(max_length=65)
 
+    def __str__(self):
+        return self.name
+
 class Recipe(models.Model):
     title = models.CharField(max_length=65)
     description = models.CharField(max_length=165)
