d4fe9b7a6bab1c2e1f7d26aea35280f241c883ce
Jhones <jhonesaly@gmail.com>
2023-03-21 13:48:13 -0300

    feat: make 'is published' work

--- django/recipes/views.py
+++ django/recipes/views.py
@@ -3,7 +3,8 @@ from utils.recipes.factory import make_recipe
 from .models import Recipe
 
 def home(request):
-    recipes = Recipe.objects.all().order_by('-id')
+    recipes = Recipe.objects.filter(is_published=True).order_by('-id')
+
     return render(request, 'recipes/pages/home.html', context={
         'recipes': recipes,
     })
