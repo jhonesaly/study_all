a2066ff8a059cb168a7e27329ecda3385367dc82
Jhones <jhonesaly@gmail.com>
2023-03-21 11:10:59 -0300

    feat: show at home created recipes

--- django/recipes/views.py
+++ django/recipes/views.py
@@ -1,10 +1,11 @@
 from django.shortcuts import render
 from utils.recipes.factory import make_recipe
-
+from .models import Recipe
 
 def home(request):
+    recipes = Recipe.objects.all().order_by('-id')
     return render(request, 'recipes/pages/home.html', context={
-        'recipes': [make_recipe() for _ in range(10)],
+        'recipes': recipes,
     })
 
 
