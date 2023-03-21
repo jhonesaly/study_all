a1de6bc3e9a46386e15d3e536dceade82c63c9c9
Jhones <jhonesaly@gmail.com>
2023-03-21 10:18:25 -0300

    feat: add title of recipe to admin CRUD

--- django/recipes/models.py
+++ django/recipes/models.py
@@ -23,3 +23,6 @@ class Recipe(models.Model):
     cover = models.ImageField(upload_to='recipes/covers/%Y/%m/%d/')
     category = models.ForeignKey(Category, on_delete=models.SET_NULL, null=True)
     author = models.ForeignKey(User, on_delete=models.SET_NULL, null=True)
+
+    def __str__(self):
+        return self.title
