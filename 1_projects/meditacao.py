ea50bf5b39b9fe079783b838a02f71a980c9503b
Jhones <jhonesaly@gmail.com>
2023-03-27 17:30:27 -0300

    feat: create function mindfulness

--- 1_projects/meditacao.py
+++ 1_projects/meditacao.py
@@ -3,3 +3,9 @@ import time
 class Meditacao:
     def __init__(self, nome):
         self.nome = nome
+
+    def mindfulness(self):
+        print("Iniciando meditação de mindfulness...")
+        time.sleep(5)
+        print("Meditação de mindfulness concluída.")
+
