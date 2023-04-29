d962eaa4f2a476d34cb43039e0412fb34ea6d03e
Jhones <jhonesaly@gmail.com>
2023-04-29 13:07:31 -0300

    feat: data manipulation

--- 1_projects/control_count/control_count.py
+++ 1_projects/control_count/control_count.py
@@ -3,3 +3,10 @@
 # transforma tamanho em MB
 # cria relatorio.txt
 
+with open('usuarios.txt', 'r') as input:
+    lines = input.readlines()
+    for line in lines:
+        name, size = line.split()
+        size_mb = round(int(size)/1024/1024,2)
+        print(name)
+        print(size_mb)
