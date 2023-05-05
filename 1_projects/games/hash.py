72d591058ade5f228d6223e82d7dd0c18b5b1a9d
Jhones <jhonesaly@gmail.com>
2023-05-05 18:31:30 -0300

    refactor: better code

--- 1_projects/games/hash.py
+++ 1_projects/games/hash.py
@@ -1,72 +1,49 @@
-hash = [['','',''],['','',''],['','','']]
-end_game = False
-
-def show(hash):
-    for line in hash:
+def show_board(board):
+    for line in board:
         print(line)
 
-def victory(hash):
-    end_game = False
+def check_victory(board):
+    for i in range(3):
+        # Checa linhas
+        if board[i][0] == board[i][1] == board[i][2] != '':
+            return f'victory of {board[i][0]}!'
+        # Checa colunas
+        if board[0][i] == board[1][i] == board[2][i] != '':
+            return f'victory of {board[0][i]}!'
+    # Checa diagonais
+    if board[0][0] == board[1][1] == board[2][2] != '':
+        return f'victory of {board[0][0]}!'
+    if board[0][2] == board[1][1] == board[2][0] != '':
+        return f'victory of {board[0][2]}!'
+    return ''
 
-    for x in range(2):
-        if hash[x][0] == hash[x][1] == hash[x][2]:
-            if hash[x][0] != '':
-                print(f'victory of {hash[x][0]}!')
-                end_game = True
-                break
-    
-        elif hash[0][x] == hash[1][x] == hash[2][x]:
-            if hash[0][x] != '':
-                print(f'victory of {hash[0][x]}!')
-                end_game = True
-                break
-
-        elif (hash[0][0] == hash[1][1] == hash[2][2]):
-            if hash[1][1] != '':
-                print(f'victory of {hash[1][1]}!')
-                end_game = True
-                break
-
-        elif (x==0) and (hash[0][2] == hash[1][1] == hash[2][0]):
-            if hash[x+1][x+1] != '':
-                print(f'victory of {hash[1][1]}!')
-                end_game = True
-                break
-
-        else:
+def make_move(board, player):
+    while True:
+        move = input(f"Digite onde desea colocar o {player} (ex: 1,1): ")
+        try:
+            x, y = [int(i) - 1 for i in move.split(',')]
+        except ValueError:
+            print('Entrada inválida. Digite novamente.')
             continue
-    
-    return end_game
-
-def move(hash, XO):
-    move_test = 'not_ok'
-
-    while move_test != 'ok':
-        move = input(f"Digite onde deja colocar o {XO} (ex: 1,1): ")
-        move = move.split(',')
-        move = [int(x)-1 for x in move]
-        if hash[move[0]][move[1]] != '':
+        if board[x][y] != '':
             print('Espaço já marcado. Escolha outro!')
         else:
-            hash[move[0]][move[1]] = XO
-            move_test = 'ok'
+            board[x][y] = player
+            break
 
-while end_game == False:
+board = [['', '', ''], ['', '', ''], ['', '', '']]
 
-    show(hash)
-
-    move(hash,'X')
-
-    end_game = victory(hash)
-
-    if end_game == True:
-        show(hash)
+while True:
+    show_board(board)
+    make_move(board, 'X')
+    result = check_victory(board)
+    if result:
+        print(result)
+        show_board(board)
         break
-
-    move(hash,'O')
-    
-    end_game = victory(hash)
-
-    if end_game == True:
-        show(hash)
+    make_move(board, 'O')
+    result = check_victory(board)
+    if result:
+        print(result)
+        show_board(board)
         break
