05a2c85b24e6da2de4f4f909fa98fae952874f9d
Jhones <jhonesaly@gmail.com>
2023-11-11 08:19:40 -0300

    docs: desestruturação

---
+++ react/board.js
@@ -0,0 +1,25 @@
+const handleGetData = async () => {
+    const userData = await fetch(`https://api.github.com/users/${user}`);
+    const responseBody = await userData.text(); // Salvar o corpo da resposta como texto
+    const newUser = JSON.parse(responseBody); // Analisar o corpo como JSON
+    
+    console.log("user:", user);
+    console.log("userData (text):", responseBody);
+    console.log("newUser (JSON):", newUser);
+    
+
+    if(newUser.name){
+      const { avatar_url, name, bio, login } = newUser;
+      //setGitUser({avatar_url, name, bio, login});
+
+      const reposData = await fetch(`https://api.github.com/users/${user}/repos`);
+      const newRepos = await reposData.json();
+
+      if (newRepos.length){
+        //setRepos(newRepos);
+      }
+    }
+  };
+
+var user = 'jhonesaly'
+handleGetData()
